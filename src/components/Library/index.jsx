d0cdcada6022a2cfa424b786f96aea763cff347b
Marion Grolleau <mariongrolleau@neuf.fr>
2022-02-02 22:28:29 +0100

    Updated show page Library

--- src/components/Library/index.jsx
+++ src/components/Library/index.jsx
@@ -1,10 +1,37 @@
-import Cards from 'components/Cards';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import SLibrary from './style';
 
 export default function Library() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:6060/cards`)
+      .then(({ data }) => {
+        setCharacters(data);
+      })
+
+      .catch(() => {
+        toast('ERROR');
+      });
+  }, []);
+
   return (
-    <>
-      <h1>Galleries d images</h1>
-      <Cards />
-    </>
+    <SLibrary>
+      <section className="card">
+        {characters.map((character) => (
+          <div className="cardCharacter">
+            <img
+              className="imgCharacter"
+              src={character.picture}
+              alt={character.name}
+            />
+            <h2 className="nameCharacter">{character.name}</h2>
+          </div>
+        ))}
+      </section>
+    </SLibrary>
   );
 }
---
+++ src/components/Library/style.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const SLibrary = styled.div`
+  .card {
+    position: absolute;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    padding: 5%;
+  }
+
+  .cardCharacter {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    border: 2px solid black;
+    border-radius: 10%;
+    width: 12vw;
+    padding: 5px;
+    background: white;
+    box-shadow: 1px 6px 13px 1px rgba(0, 0, 0, 0.42);
+    -webkit-box-shadow: 1px 6px 13px 1px rgba(0, 0, 0, 0.42);
+    -moz-box-shadow: 1px 6px 13px 1px rgba(0, 0, 0, 0.42);
+  }
+
+  .imgCharacter {
+    position: relative;
+    height: 20vh;
+    padding-top: 30px;
+  }
+
+  .nameCharacter {
+    position: absolute;
+    display: flex;
+    margin: 5px;
+    font-family: var(--font-family);
+    font-size: 1.5rem;
+  }
+`;
+export default SLibrary;
